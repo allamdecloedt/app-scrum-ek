8ee9fc572dba0706b36b46160053f973eb7dc515
allamdecloedt <abdelfattah.allam@decloedt.cloud>
2024-03-07 16:43:50 +0100

    1st v app

---
+++ application/logs/log-2024-02-23.php
@@ -0,0 +1,97 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-02-23 15:24:43 --> 404 Page Not Found: Assets/js
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-02-23 15:24:43 --> 404 Page Not Found: Assets/js
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-02-23 15:24:43 --> 404 Page Not Found: Assets/js
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-02-23 15:24:43 --> 404 Page Not Found: Assets/js
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-02-23 15:24:43 --> 404 Page Not Found: Assets/js
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-02-23 15:24:43 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:43 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:43 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:43 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:43 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:43 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:43 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:43 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:43 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:43 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:43 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:43 --> 404 Page Not Found: Uploads/system
+ERROR - 2024-02-23 15:24:47 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:47 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:47 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:47 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:47 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:47 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:47 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:47 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:47 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:47 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:47 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:47 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:47 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:47 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:50 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:50 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:50 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:50 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:50 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:50 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:50 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:50 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:50 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:50 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:24:50 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:27:06 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:27:06 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:27:06 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:27:06 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:27:06 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:27:06 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:27:06 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:27:06 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:27:18 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:27:18 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:27:18 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:27:18 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:27:18 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:27:18 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:27:18 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:27:18 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:31:42 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:31:42 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:31:42 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:31:42 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:31:42 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:31:42 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:31:42 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:31:42 --> 404 Page Not Found: Assets/js
+ERROR - 2024-02-23 15:34:33 --> Severity: Warning --> Undefined array key "educational_qualifications" C:\xampp\htdocs\ekattor_7.6\application\models\Frontend_model.php 499
+ERROR - 2024-02-23 15:34:33 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\models\Frontend_model.php 499
+ERROR - 2024-02-23 16:02:15 --> Severity: Warning --> file_get_contents(uploads/addons/codecanyon-EHCNB0C2-ekattor-online-course-addon/config.json): Failed to open stream: No such file or directory C:\xampp\htdocs\ekattor_7.6\application\models\Addon_model.php 51
+ERROR - 2024-02-23 16:02:15 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\models\Addon_model.php 77
+ERROR - 2024-02-23 16:02:15 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\models\Addon_model.php 78
+ERROR - 2024-02-23 16:02:15 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\models\Addon_model.php 79
+ERROR - 2024-02-23 16:02:15 --> Query error: Column 'name' cannot be null - Invalid query: INSERT INTO `addons` (`name`, `unique_identifier`, `version`, `status`, `created_at`) VALUES (NULL, NULL, NULL, 1, 1677193200)
+ERROR - 2024-02-23 16:12:53 --> Severity: Warning --> Undefined variable $lesson_id C:\xampp\htdocs\ekattor_7.6\application\views\lessons\common_scripts.php 6
+ERROR - 2024-02-23 16:12:53 --> Severity: Warning --> Undefined variable $lesson_details C:\xampp\htdocs\ekattor_7.6\application\views\lessons\common_scripts.php 7
+ERROR - 2024-02-23 16:12:53 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\views\lessons\common_scripts.php 7
+ERROR - 2024-02-23 16:12:53 --> Severity: Warning --> Undefined variable $provider C:\xampp\htdocs\ekattor_7.6\application\views\lessons\common_scripts.php 8
+ERROR - 2024-02-23 16:12:53 --> Severity: Warning --> Undefined variable $lesson_id C:\xampp\htdocs\ekattor_7.6\application\views\lessons\common_scripts.php 63
+ERROR - 2024-02-23 16:13:04 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\views\backend\academy\edit.php 16
+ERROR - 2024-02-23 16:13:13 --> Severity: error --> Exception: sizeof(): Argument #1 ($value) must be of type Countable|array, null given C:\xampp\htdocs\ekattor_7.6\application\models\addons\Lms_model.php 228
+ERROR - 2024-02-23 16:13:18 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\views\backend\academy\edit.php 16
+ERROR - 2024-02-23 16:19:54 --> Severity: error --> Exception: sizeof(): Argument #1 ($value) must be of type Countable|array, null given C:\xampp\htdocs\ekattor_7.6\application\models\addons\Lms_model.php 228
+ERROR - 2024-02-23 16:27:14 --> Severity: Warning --> Undefined array key "smtp_crypto" C:\xampp\htdocs\ekattor_7.6\application\helpers\common_helper.php 17
+ERROR - 2024-02-23 16:27:24 --> Severity: Warning --> fsockopen(): Unable to connect to ssl://smtp.gmail.com:465 (Une tentative de connexion a échoué car le parti connecté n’a pas répondu convenablement au-delà d’une certaine durée ou une connexion établie a échoué car l’hôte de connexion n’a pas répondu) C:\xampp\htdocs\ekattor_7.6\system\libraries\Email.php 2069
+ERROR - 2024-02-23 16:27:24 --> Severity: Warning --> Undefined array key "smtp_crypto" C:\xampp\htdocs\ekattor_7.6\application\helpers\common_helper.php 17
---
+++ application/logs/log-2024-03-01.php
@@ -0,0 +1,76 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-03-01 14:38:33 --> 404 Page Not Found: Hom6e/index
+ERROR - 2024-03-01 15:00:20 --> 404 Page Not Found: Homeff/index
+ERROR - 2024-03-01 15:07:48 --> 404 Page Not Found: Assets/frontend
+ERROR - 2024-03-01 15:09:48 --> Severity: Warning --> include(homeccc.php): Failed to open stream: No such file or directory C:\xampp\htdocs\ekattor_7.6\application\views\frontend\ultimate\index.php 16
+ERROR - 2024-03-01 15:09:48 --> Severity: Warning --> include(): Failed opening 'homeccc.php' for inclusion (include_path='\xampp\php\PEAR') C:\xampp\htdocs\ekattor_7.6\application\views\frontend\ultimate\index.php 16
+ERROR - 2024-03-01 15:10:14 --> Severity: Warning --> include(homeccc.php): Failed to open stream: No such file or directory C:\xampp\htdocs\ekattor_7.6\application\views\frontend\ultimate\index.php 16
+ERROR - 2024-03-01 15:10:14 --> Severity: Warning --> include(): Failed opening 'homeccc.php' for inclusion (include_path='\xampp\php\PEAR') C:\xampp\htdocs\ekattor_7.6\application\views\frontend\ultimate\index.php 16
+ERROR - 2024-03-01 15:10:24 --> Severity: Warning --> include(homeccc.php): Failed to open stream: No such file or directory C:\xampp\htdocs\ekattor_7.6\application\views\frontend\ultimate\index.php 16
+ERROR - 2024-03-01 15:10:24 --> Severity: Warning --> include(): Failed opening 'homeccc.php' for inclusion (include_path='\xampp\php\PEAR') C:\xampp\htdocs\ekattor_7.6\application\views\frontend\ultimate\index.php 16
+ERROR - 2024-03-01 15:10:28 --> 404 Page Not Found: Homeccc/index
+ERROR - 2024-03-01 15:12:41 --> 404 Page Not Found: Homeccc/index
+ERROR - 2024-03-01 15:12:45 --> Severity: Warning --> include(homeccc.php): Failed to open stream: No such file or directory C:\xampp\htdocs\ekattor_7.6\application\views\frontend\ultimate\index.php 16
+ERROR - 2024-03-01 15:12:45 --> Severity: Warning --> include(): Failed opening 'homeccc.php' for inclusion (include_path='\xampp\php\PEAR') C:\xampp\htdocs\ekattor_7.6\application\views\frontend\ultimate\index.php 16
+ERROR - 2024-03-01 15:13:11 --> 404 Page Not Found: Homeccc/index
+ERROR - 2024-03-01 15:13:14 --> 404 Page Not Found: Homeccc/index
+ERROR - 2024-03-01 15:13:16 --> 404 Page Not Found: Homeccc/index
+ERROR - 2024-03-01 15:14:02 --> Severity: Warning --> include(home.php): Failed to open stream: No such file or directory C:\xampp\htdocs\ekattor_7.6\application\views\frontend\ultimate\index.php 16
+ERROR - 2024-03-01 15:14:02 --> Severity: Warning --> include(): Failed opening 'home.php' for inclusion (include_path='\xampp\php\PEAR') C:\xampp\htdocs\ekattor_7.6\application\views\frontend\ultimate\index.php 16
+ERROR - 2024-03-01 15:14:27 --> Severity: Warning --> include(home.php): Failed to open stream: No such file or directory C:\xampp\htdocs\ekattor_7.6\application\views\frontend\ultimate\index.php 16
+ERROR - 2024-03-01 15:14:27 --> Severity: Warning --> include(): Failed opening 'home.php' for inclusion (include_path='\xampp\php\PEAR') C:\xampp\htdocs\ekattor_7.6\application\views\frontend\ultimate\index.php 16
+ERROR - 2024-03-01 15:21:10 --> Severity: Warning --> file_get_contents(./update/Aménagements extérieurs de prestige./update_config.json): Failed to open stream: No such file or directory C:\xampp\htdocs\ekattor_7.6\application\controllers\Updater.php 70
+ERROR - 2024-03-01 15:21:10 --> Severity: Warning --> require(./update/Aménagements extérieurs de prestige./update_script.php): Failed to open stream: No such file or directory C:\xampp\htdocs\ekattor_7.6\application\controllers\Updater.php 75
+ERROR - 2024-03-01 15:21:10 --> Severity: error --> Exception: Failed opening required './update/Aménagements extérieurs de prestige./update_script.php' (include_path='\xampp\php\PEAR') C:\xampp\htdocs\ekattor_7.6\application\controllers\Updater.php 75
+ERROR - 2024-03-01 16:05:26 --> 404 Page Not Found: Homecc/index
+ERROR - 2024-03-01 16:11:41 --> Severity: Warning --> Undefined array key "dark_logo" C:\xampp\htdocs\ekattor_7.6\application\models\Settings_model.php 48
+ERROR - 2024-03-01 16:11:41 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\models\Settings_model.php 48
+ERROR - 2024-03-01 16:11:41 --> Severity: Warning --> Undefined array key "light_logo" C:\xampp\htdocs\ekattor_7.6\application\models\Settings_model.php 51
+ERROR - 2024-03-01 16:11:41 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\models\Settings_model.php 51
+ERROR - 2024-03-01 16:11:41 --> Severity: Warning --> Undefined array key "small_logo" C:\xampp\htdocs\ekattor_7.6\application\models\Settings_model.php 54
+ERROR - 2024-03-01 16:11:41 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\models\Settings_model.php 54
+ERROR - 2024-03-01 16:11:41 --> Severity: Warning --> Undefined array key "favicon" C:\xampp\htdocs\ekattor_7.6\application\models\Settings_model.php 57
+ERROR - 2024-03-01 16:11:41 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\models\Settings_model.php 57
+ERROR - 2024-03-01 16:11:45 --> Severity: Warning --> Undefined array key "dark_logo" C:\xampp\htdocs\ekattor_7.6\application\models\Settings_model.php 48
+ERROR - 2024-03-01 16:11:45 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\models\Settings_model.php 48
+ERROR - 2024-03-01 16:11:45 --> Severity: Warning --> Undefined array key "light_logo" C:\xampp\htdocs\ekattor_7.6\application\models\Settings_model.php 51
+ERROR - 2024-03-01 16:11:45 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\models\Settings_model.php 51
+ERROR - 2024-03-01 16:11:45 --> Severity: Warning --> Undefined array key "small_logo" C:\xampp\htdocs\ekattor_7.6\application\models\Settings_model.php 54
+ERROR - 2024-03-01 16:11:45 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\models\Settings_model.php 54
+ERROR - 2024-03-01 16:11:45 --> Severity: Warning --> Undefined array key "favicon" C:\xampp\htdocs\ekattor_7.6\application\models\Settings_model.php 57
+ERROR - 2024-03-01 16:11:45 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\models\Settings_model.php 57
+ERROR - 2024-03-01 16:13:46 --> Severity: Warning --> Undefined array key "dark_logo" C:\xampp\htdocs\ekattor_7.6\application\models\Settings_model.php 48
+ERROR - 2024-03-01 16:13:46 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\models\Settings_model.php 48
+ERROR - 2024-03-01 16:13:46 --> Severity: Warning --> Undefined array key "light_logo" C:\xampp\htdocs\ekattor_7.6\application\models\Settings_model.php 51
+ERROR - 2024-03-01 16:13:46 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\models\Settings_model.php 51
+ERROR - 2024-03-01 16:13:46 --> Severity: Warning --> Undefined array key "small_logo" C:\xampp\htdocs\ekattor_7.6\application\models\Settings_model.php 54
+ERROR - 2024-03-01 16:13:46 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\models\Settings_model.php 54
+ERROR - 2024-03-01 16:13:46 --> Severity: Warning --> Undefined array key "favicon" C:\xampp\htdocs\ekattor_7.6\application\models\Settings_model.php 57
+ERROR - 2024-03-01 16:13:46 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\models\Settings_model.php 57
+ERROR - 2024-03-01 16:14:37 --> 404 Page Not Found: Homecc/index
+ERROR - 2024-03-01 16:14:41 --> 404 Page Not Found: Assets/frontend
+ERROR - 2024-03-01 16:19:38 --> 404 Page Not Found: Assets/frontend
+ERROR - 2024-03-01 16:24:27 --> 404 Page Not Found: Assets/frontend
+ERROR - 2024-03-01 16:25:48 --> 404 Page Not Found: Assets/frontend
+ERROR - 2024-03-01 16:26:36 --> 404 Page Not Found: Assets/frontend
+ERROR - 2024-03-01 16:30:07 --> Severity: Warning --> Undefined array key "educational_qualifications" C:\xampp\htdocs\ekattor_7.6\application\models\Frontend_model.php 499
+ERROR - 2024-03-01 16:30:07 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\models\Frontend_model.php 499
+ERROR - 2024-03-01 16:30:43 --> Severity: Warning --> Undefined array key "smtp_crypto" C:\xampp\htdocs\ekattor_7.6\application\helpers\common_helper.php 17
+ERROR - 2024-03-01 16:30:43 --> Severity: Warning --> Undefined array key "smtp_crypto" C:\xampp\htdocs\ekattor_7.6\application\helpers\common_helper.php 17
+ERROR - 2024-03-01 16:32:28 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\views\backend\superadmin\permission\list.php 21
+ERROR - 2024-03-01 16:32:28 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\views\backend\superadmin\permission\list.php 21
+ERROR - 2024-03-01 16:32:28 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\views\backend\superadmin\permission\list.php 25
+ERROR - 2024-03-01 16:32:28 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\views\backend\superadmin\permission\list.php 25
+ERROR - 2024-03-01 16:32:28 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\views\backend\superadmin\permission\list.php 29
+ERROR - 2024-03-01 16:32:28 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\views\backend\superadmin\permission\list.php 29
+ERROR - 2024-03-01 16:32:28 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\views\backend\superadmin\permission\list.php 33
+ERROR - 2024-03-01 16:32:28 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\views\backend\superadmin\permission\list.php 33
+ERROR - 2024-03-01 16:32:32 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\views\backend\superadmin\permission\list.php 21
+ERROR - 2024-03-01 16:32:32 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\views\backend\superadmin\permission\list.php 21
+ERROR - 2024-03-01 16:32:32 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\views\backend\superadmin\permission\list.php 25
+ERROR - 2024-03-01 16:32:32 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\views\backend\superadmin\permission\list.php 25
+ERROR - 2024-03-01 16:32:32 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\views\backend\superadmin\permission\list.php 29
+ERROR - 2024-03-01 16:32:32 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\views\backend\superadmin\permission\list.php 29
+ERROR - 2024-03-01 16:32:32 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\views\backend\superadmin\permission\list.php 33
+ERROR - 2024-03-01 16:32:32 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\ekattor_7.6\application\views\backend\superadmin\permission\list.php 33
