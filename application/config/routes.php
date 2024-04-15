7b26e6af4c0fd037f296f98aca5da43c02728c57
allamdecloedt <abdelfattah.allam@decloedt.cloud>
2024-04-15 16:01:57 +0100

    SM-11 #comment  ajouter api menu

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,4 +51,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 */
 $route['default_controller'] = 'home';
 $route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
+$route['translate_uri_dashes'] = TRUE;
+
+
+$route['api/menu'] = 'api/Admin/menu';
+
