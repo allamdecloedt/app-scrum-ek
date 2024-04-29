829b2455d25bcad61f0e73e42e075079cb00e547
allamdecloedt <abdelfattah.allam@decloedt.cloud>
2024-04-29 14:31:14 +0100

    SM-25  #comment  add new colonne

---
+++ assets/2024-04-29_new_colonne_school.sql
@@ -0,0 +1,11 @@
+ALTER TABLE schools
+ADD description VARCHAR(255);
+
+ALTER TABLE schools
+ADD access int(11);
+
+ALTER TABLE schools
+ADD thumbnail VARCHAR(255);
+
+ALTER TABLE schools
+ADD category VARCHAR(255);
