240557e45e308916a119435f4771bd831835db5f
allamdecloedt <abdelfattah.allam@decloedt.cloud>
2024-05-29 10:09:48 +0100

    Remove files from Git tracking and update .gitignore

--- application/logs/log-2024-02-27.php
+++
@@ -1,6 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2024-02-27 09:18:36 --> Severity: error --> Exception: date(): Argument #2 ($timestamp) must be of type ?int, string given C:\xampp\htdocs\ekattor_7.6\application\views\backend\superadmin\student\update.php 77
-ERROR - 2024-02-27 09:19:08 --> Severity: error --> Exception: date(): Argument #2 ($timestamp) must be of type ?int, string given C:\xampp\htdocs\ekattor_7.6\application\views\backend\superadmin\student\update.php 77
-ERROR - 2024-02-27 09:19:14 --> Severity: error --> Exception: date(): Argument #2 ($timestamp) must be of type ?int, string given C:\xampp\htdocs\ekattor_7.6\application\views\backend\superadmin\student\update.php 77
-ERROR - 2024-02-27 09:26:51 --> Severity: Warning --> Undefined array key "smtp_crypto" C:\xampp\htdocs\ekattor_7.6\application\helpers\common_helper.php 17
--- application/logs/log-2024-03-05.php
+++
@@ -1,4 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2024-03-05 11:24:35 --> Severity: Warning --> Undefined array key "smtp_crypto" C:\xampp\htdocs\ekattor_7.6\application\helpers\common_helper.php 17
-ERROR - 2024-03-05 11:24:36 --> Severity: Warning --> Undefined array key "smtp_crypto" C:\xampp\htdocs\ekattor_7.6\application\helpers\common_helper.php 17
