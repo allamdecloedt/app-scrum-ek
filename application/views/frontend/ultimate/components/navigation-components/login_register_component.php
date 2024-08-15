6e506b379f1fef5d7b70698aa4527432fee6e0c8
allamdecloedt <abdelfattah.allam@decloedt.cloud>
2024-08-15 14:46:38 +0100

    SM-98  #comment  add forget password

--- application/views/frontend/ultimate/components/navigation-components/login_register_component.php
+++ application/views/frontend/ultimate/components/navigation-components/login_register_component.php
@@ -30,6 +30,12 @@
             <span class="ml-1  register-link"><span>(</span> <?php echo get_phrase("register") ?><span
                     class="ml-1">)</span></span>
         </a>
+
+        <a class="forget-phrase text-uppercase">
+            <?php echo get_phrase("Forgot account?") ?>
+            <span class="ml-1  forget-link"><span>(</span> <?php echo get_phrase("forget password") ?><span
+                    class="ml-1">)</span></span>
+        </a>
     </div>
 
     <!-- Register Section -->
@@ -148,7 +154,49 @@
             </div>
             <!-- Third Register Form Section End-->
     </div>
-    </form>
+        </form>
+
+<!-- forget Section -->
+        <div class="forget-dropdown hidden-section display-none">
+            <svg class="login-exit-svg" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                <path fill-rule="evenodd"
+                    d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z" />
+                <path fill-rule="evenodd"
+                    d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z" />
+            </svg>
+            <a class="text-uppercase">
+                <span class="loginforge-link">
+                    <svg class="m-1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-arrow-bar-left" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                            d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5M10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5" />
+                    </svg>
+                    <?php echo get_phrase("login") ?>
+                </span>
+            </a>
+
+
+        
+            <form class="forget-form mt-10" id="forget-form" method="post" enctype="multipart/form-data"
+                action="<?php echo site_url('login/retrieve_password_site'); ?>">
+
+                <!-- First forget Form Section -->
+                <!-- <div class="tab"> -->
+                    <div class="mb-4 login-input">
+                        <label for="forgetEmail"
+                            class="login-input-label text-uppercase"><?php echo get_phrase("Email") ?></label>
+                        <input type="text" class="form-control shadow-none information" id="forgotEmail"
+                            name="email" required data-msg="<?php echo get_phrase("required") ?>">
+                    </div>
+                    <button type="submit" id="registerSubmit" class="register-button text-uppercase">
+                    <?php echo get_phrase("sent_password_reset_link") ?>
+                    </button>
+                <!-- </div> -->
+                <!-- First forget Form Section End-->
+            </form>
+        </div>
+       
 </div>
 </div>
 </div>
