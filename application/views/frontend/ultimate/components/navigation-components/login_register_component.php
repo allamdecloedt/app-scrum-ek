9317e5e9539daff8813e5c2ce67d988067ee5300
Arran-Decleodt <arranvanaerschot@decloedt.cloud>
2024-09-26 19:15:41 +0200

    seperate notification by case

--- application/views/frontend/ultimate/components/navigation-components/login_register_component.php
+++ application/views/frontend/ultimate/components/navigation-components/login_register_component.php
@@ -11,15 +11,16 @@
         <form class="login-form mt-10" id="login-form" action="<?php echo site_url('login/validate_login_frontend'); ?>"
             method="post">
             <div class="mb-4 mt-4 login-input">
-                <label for="loginEmail"
-                    class="login-input-label text-uppercase"><?php echo get_phrase("e-mail") ?><span class="required"> * </span></label>
+                <label for="loginEmail" class="login-input-label text-uppercase"><?php echo get_phrase("e-mail") ?><span
+                        class="required"> * </span></label>
                 <input type="email" class="form-control shadow-none" id="loginEmail" aria-describedby="emailHelp"
                     name="login_email">
 
             </div>
             <div class="mb-3 login-input">
                 <label for="loginPassword"
-                    class=" login-input-label text-uppercase"><?php echo get_phrase("password") ?><span class="required"> * </span></label>
+                    class=" login-input-label text-uppercase"><?php echo get_phrase("password") ?><span
+                        class="required"> * </span></label>
                 <input type="password" class="form-control shadow-none" id="loginPassword" name="login_password">
             </div>
             <button type="submit" id="loginSubmit"
@@ -74,19 +75,22 @@
             <div class="tab">
                 <div class="mb-4 login-input">
                     <label for="registerFirstName"
-                        class="login-input-label text-uppercase"><?php echo get_phrase("first name") ?><span class="required"> * </span></label>
+                        class="login-input-label text-uppercase"><?php echo get_phrase("first name") ?><span
+                            class="required"> * </span></label>
                     <input type="text" class="form-control shadow-none information" id="registerFirstName"
                         name="register_first_name" required data-msg="<?php echo get_phrase("required") ?>">
                 </div>
                 <div class="mb-4 login-input">
                     <label for="registerLastName"
-                        class=" login-input-label text-uppercase"><?php echo get_phrase("last name") ?><span class="required"> * </span></label>
+                        class=" login-input-label text-uppercase"><?php echo get_phrase("last name") ?><span
+                            class="required"> * </span></label>
                     <input id="registerLastName" type="text" class="form-control shadow-none information" id=""
                         name="register_last_name" required data-msg="<?php echo get_phrase("required") ?>">
                 </div>
                 <div class="mb-4 login-input">
                     <label for="registerEmail"
-                        class=" login-input-label text-uppercase"><?php echo get_phrase("e-mail") ?><span class="required"> * </span></label>
+                        class=" login-input-label text-uppercase"><?php echo get_phrase("e-mail") ?><span
+                            class="required"> * </span></label>
                     <input id="registerEmail" type="email" class="form-control shadow-none information" id=""
                         name="register_email" required data-msg="<?php echo get_phrase("required") ?>">
                 </div>
@@ -98,14 +102,16 @@
 
                 <div class="mb-4 login-input">
                     <label for="register_date_of_birth"
-                        class=" login-input-label text-uppercase"><?php echo get_phrase("date of birth") ?><span class="required"> * </span></label>
+                        class=" login-input-label text-uppercase"><?php echo get_phrase("date of birth") ?><span
+                            class="required"> * </span></label>
                     <input onfocus="'showPicker' in this && this.showPicker()" type="date"
                         class="form-control  shadow-none information" id="register_date_of_birth"
                         name="register_date_of_birth" required data-msg="<?php echo get_phrase("required") ?>">
                 </div>
                 <div class="mb-4 login-input">
                     <label for="register_gender"
-                        class=" login-input-label text-uppercase"><?php echo get_phrase("gender") ?><span class="required"> * </span></label>
+                        class=" login-input-label text-uppercase"><?php echo get_phrase("gender") ?><span
+                            class="required"> * </span></label>
                     <select name="register_gender" id="register_gender"
                         class="form-control rounded-end shadow-none information" required
                         data-msg="<?php echo get_phrase("required") ?>">
@@ -123,7 +129,7 @@
                                 d="M14 4.5V14a2 2 0 0 1-2 2v-1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5zm-3.76 8.132q.114.23.14.492h-.776a.8.8 0 0 0-.097-.249.7.7 0 0 0-.17-.19.7.7 0 0 0-.237-.126 1 1 0 0 0-.299-.044q-.427 0-.665.302-.234.301-.234.85v.498q0 .351.097.615a.9.9 0 0 0 .304.413.87.87 0 0 0 .519.146 1 1 0 0 0 .457-.096.67.67 0 0 0 .272-.264q.09-.164.091-.363v-.255H8.82v-.59h1.576v.798q0 .29-.097.55a1.3 1.3 0 0 1-.293.458 1.4 1.4 0 0 1-.495.313q-.296.111-.697.111a2 2 0 0 1-.753-.132 1.45 1.45 0 0 1-.533-.377 1.6 1.6 0 0 1-.32-.58 2.5 2.5 0 0 1-.105-.745v-.506q0-.543.2-.95.201-.406.582-.633.384-.228.926-.228.357 0 .636.1.281.1.48.275.2.176.314.407Zm-8.64-.706H0v4h.791v-1.343h.803q.43 0 .732-.172.305-.177.463-.475a1.4 1.4 0 0 0 .161-.677q0-.374-.158-.677a1.2 1.2 0 0 0-.46-.477q-.3-.18-.732-.179m.545 1.333a.8.8 0 0 1-.085.381.57.57 0 0 1-.238.24.8.8 0 0 1-.375.082H.788v-1.406h.66q.327 0 .512.182.185.181.185.521m1.964 2.666V13.25h.032l1.761 2.675h.656v-3.999h-.75v2.66h-.032l-1.752-2.66h-.662v4z" />
                         </svg>
                         <span class="file-name-photo file-name"><?php echo get_phrase('picture'); ?>...</span>
-                        <span class="required"> * </span>  </label>
+                        <span class="required"> * </span> </label>
                     <input id="student_image_upload" type="file" class="inputfile" name="student_image_upload"
                         accept=".jpg, .jpeg, .png" required>
                 </div>
@@ -135,14 +141,16 @@
                 <div class="register-form-section pb-4">
                     <div class="mb-4 pt-3 login-input">
                         <label for="registerPassword"
-                            class="login-input-label text-uppercase"><?php echo get_phrase("password") ?><span class="required"> * </span></label>
+                            class="login-input-label text-uppercase"><?php echo get_phrase("password") ?><span
+                                class="required"> * </span></label>
                         <input oninput="checkPassword()" type="password" class="form-control shadow-none password"
                             id="registerPassword" name="register_password"
                             data-msg="<?php echo get_phrase("required") ?>" required>
                     </div>
                     <div class=" mb-3 pt-2  login-input">
                         <label for="registerRepeatPassword"
-                            class="login-input-label text-uppercase"><?php echo get_phrase("repeat password") ?><span class="required"> * </span></label>
+                            class="login-input-label text-uppercase"><?php echo get_phrase("repeat password") ?><span
+                                class="required"> * </span></label>
                         <input oninput="checkPassword()" type="password" class="form-control shadow-none password"
                             id="registerRepeatPassword" name="register_repeat_password"
                             data-msg="<?php echo get_phrase("required") ?>" required>
@@ -154,49 +162,49 @@
             </div>
             <!-- Third Register Form Section End-->
     </div>
+    </form>
+
+    <!-- forget Section -->
+    <div class="forget-dropdown hidden-section display-none">
+        <svg class="login-exit-svg" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+            class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+            <path fill-rule="evenodd"
+                d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z" />
+            <path fill-rule="evenodd"
+                d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z" />
+        </svg>
+        <a class="text-uppercase">
+            <span class="loginforge-link">
+                <svg class="m-1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="bi bi-arrow-bar-left" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5M10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5" />
+                </svg>
+                <?php echo get_phrase("login") ?>
+            </span>
+        </a>
+
+
+
+        <form class="forget-form mt-10" id="forget-form" method="post" enctype="multipart/form-data"
+            action="<?php echo site_url('login/send_reset_link'); ?>">
+
+            <!-- First forget Form Section -->
+            <!-- <div class="tab"> -->
+            <div class="mb-4 login-input">
+                <label for="forgetEmail" class="login-input-label text-uppercase"><?php echo get_phrase("Email") ?><span
+                        class="required"> * </span></label>
+                <input type="text" class="form-control shadow-none information" id="forgotEmail" name="email" required
+                    data-msg="<?php echo get_phrase("required") ?>">
+            </div>
+            <button type="submit" id="registerSubmit" class="register-button text-uppercase">
+                <?php echo get_phrase("sent_password_reset_link") ?>
+            </button>
+            <!-- </div> -->
+            <!-- First forget Form Section End-->
         </form>
+    </div>
 
-<!-- forget Section -->
-        <div class="forget-dropdown hidden-section display-none">
-            <svg class="login-exit-svg" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                class="bi bi-box-arrow-right" viewBox="0 0 16 16">
-                <path fill-rule="evenodd"
-                    d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z" />
-                <path fill-rule="evenodd"
-                    d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z" />
-            </svg>
-            <a class="text-uppercase">
-                <span class="loginforge-link">
-                    <svg class="m-1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                        class="bi bi-arrow-bar-left" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd"
-                            d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5M10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5" />
-                    </svg>
-                    <?php echo get_phrase("login") ?>
-                </span>
-            </a>
-
-
-        
-            <form class="forget-form mt-10" id="forget-form" method="post" enctype="multipart/form-data"
-                action="<?php echo site_url('login/send_reset_link'); ?>">
-
-                <!-- First forget Form Section -->
-                <!-- <div class="tab"> -->
-                    <div class="mb-4 login-input">
-                        <label for="forgetEmail"
-                            class="login-input-label text-uppercase"><?php echo get_phrase("Email") ?><span class="required"> * </span></label>
-                        <input type="text" class="form-control shadow-none information" id="forgotEmail"
-                            name="email" required data-msg="<?php echo get_phrase("required") ?>">
-                    </div>
-                    <button type="submit" id="registerSubmit" class="register-button text-uppercase">
-                    <?php echo get_phrase("sent_password_reset_link") ?>
-                    </button>
-                <!-- </div> -->
-                <!-- First forget Form Section End-->
-            </form>
-        </div>
-       
 </div>
 </div>
 </div>
@@ -285,57 +293,103 @@
     }
 
 
-   document.getElementById("loginSubmit").addEventListener("click", function (event) {
-       if(!loginSubmit()){
-              event.preventDefault();
-       }
+
+
+
+
+
+    document.getElementById("loginSubmit").addEventListener("click", function (event) {
+        if (!loginSubmit()) {
+            event.preventDefault();
+        }
     });
 
 
     function loginSubmit() {
+
         var email = document.getElementById("loginEmail").value;
         var password = document.getElementById("loginPassword").value;
 
-         validateCredentials(email, password).then((status) => {
+       
+
+        emailExists(email).then((status) => {
             if (status) {
-                document.getElementById("login-form").submit();
-                return true;
+                error_notify('<?php echo get_phrase("E-mail_address_not_in_use") ?>');
             }
             else {
-                return false;
-                
+                 validateCredentials(email, password).then((status) => {
+                if (status) {
+                    document.getElementById("login-form").submit();
+                }
+                else {
+                    error_notify('<?php echo get_phrase("credentials_incorrect") ?>');
+
+                }
+            });
             }
+        }) 
+
+        
+
+    }
+
+
+
+    function emailExists(email) {
+        return new Promise((resolve, reject) => {
+            var emailInput = document.getElementById("loginEmail");
+
+            fetch('<?php echo base_url("register/validate_email"); ?>', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email: email }),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.status == true) {
+
+                        resolve(true);
+                    } else {
+                        resolve(false);
+                    }
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                    resolve(false);
+                });
+
         });
     }
 
-    
 
-     function validateCredentials(email, password) {
+
+    function validateCredentials(email, password) {
         return new Promise((resolve, reject) => {
             var emailInput = document.getElementById("loginEmail");
             var passwordInput = document.getElementById("loginPassword");
 
-                fetch('<?php echo base_url("login/validate_credentials"); ?>', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({ email: email, password: password }),
-                })
-                    .then(response => response.json())
-                    .then(data => {
-                        if (data.status == false) {
-                            error_notify('<?php echo get_phrase("credentials_incorrect") ?>');
-                            resolve(false);
-                        } else {
-                            resolve(true);
-                        }
-                    })
-                    .catch((error) => {
-                        console.error('Error:', error);
+            fetch('<?php echo base_url("login/validate_credentials"); ?>', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email: email, password: password }),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.status == false) {
                         resolve(false);
-                    });
-            
+                    } else {
+                        resolve(true);
+                    }
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                    resolve(false);
+                });
+
         });
     }
 
