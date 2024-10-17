96147ec080bfb02ce6a78ca8dee29638b025694b
MaskiAymen <aymenmaski33@gmail.com>
2024-10-17 09:19:31 +0100

    Inscription

--- application/config/routes.php
+++ application/config/routes.php
@@ -275,3 +275,5 @@ $route['api/invoices/delete/(:num)'] = 'api/Admin/delete_invoice/$1';
 
 
 $route['api/GetAppropriateCourses'] = 'api/Admin/get_appropriate_courses';
+//register 
+$route['api/Register']  = 'api/Admin/register';
