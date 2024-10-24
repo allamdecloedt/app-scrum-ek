6a7f0c628a47e90679e1b7e45c8ef19aa0c3a906
allamdecloedt <abdelfattah.allam@decloedt.cloud>
2024-10-24 16:14:31 +0100

    SM-149  #comment  fix bug uplade image

--- application/views/frontend/ultimate/online_admission.php
+++ application/views/frontend/ultimate/online_admission.php
@@ -340,37 +340,34 @@
             </div>
           </div>
         </div>
-        <div class="col-11 col-sm-8 col-lg-4   ">
+
+
+        <div class="col-11 col-sm-8 col-lg-4">
           <div class="js-form-message">
             <div class="mb-3">
-              <p class="pb-3 form-label text-white text-end"><?php echo get_phrase('your_photo'); ?> <span
-                  class="text-danger">*</span>
+              <p class="pb-3 form-label text-white " style="text-align: center;"><?php echo get_phrase('your_photo'); ?> 
+                <span class="text-danger">*</span>
               </p>
-              <div id="photo-preview" class="">
-                <a role="button" class="btn photo-preview-btn disabled">
-                  <svg class="m-1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                    class="bi bi-eye-fill" viewBox="0 0 16 16">
-                    <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0" />
-                    <path
-                      d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7" />
-                  </svg>
-                </a>
+              <div id="photo-preview" class="photo-preview">
+                <!-- L'image sélectionnée apparaîtra ici -->
+                <img src="<?php echo base_url() . 'uploads/users/placeholder.jpg' ?>" alt="Default Avatar" id="default-avatar">
+
               </div>
-              <label for="student_image" class="btn btn-sm button-label form-label text-white ">
-                <svg class="" xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor"
+              
+              <label for="student_image" class="btn btn-sm button-label form-label text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor"
                   class="bi bi-filetype-png" viewBox="0 0 16 16">
                   <path fill-rule="evenodd"
-                    d="M14 4.5V14a2 2 0 0 1-2 2v-1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5zm-3.76 8.132q.114.23.14.492h-.776a.8.8 0 0 0-.097-.249.7.7 0 0 0-.17-.19.7.7 0 0 0-.237-.126 1 1 0 0 0-.299-.044q-.427 0-.665.302-.234.301-.234.85v.498q0 .351.097.615a.9.9 0 0 0 .304.413.87.87 0 0 0 .519.146 1 1 0 0 0 .457-.096.67.67 0 0 0 .272-.264q.09-.164.091-.363v-.255H8.82v-.59h1.576v.798q0 .29-.097.55a1.3 1.3 0 0 1-.293.458 1.4 1.4 0 0 1-.495.313q-.296.111-.697.111a2 2 0 0 1-.753-.132 1.45 1.45 0 0 1-.533-.377 1.6 1.6 0 0 1-.32-.58 2.5 2.5 0 0 1-.105-.745v-.506q0-.543.2-.95.201-.406.582-.633.384-.228.926-.228.357 0 .636.1.281.1.48.275.2.176.314.407Zm-8.64-.706H0v4h.791v-1.343h.803q.43 0 .732-.172.305-.177.463-.475a1.4 1.4 0 0 0 .161-.677q0-.374-.158-.677a1.2 1.2 0 0 0-.46-.477q-.3-.18-.732-.179m.545 1.333a.8.8 0 0 1-.085.381.57.57 0 0 1-.238.24.8.8 0 0 1-.375.082H.788v-1.406h.66q.327 0 .512.182.185.181.185.521m1.964 2.666V13.25h.032l1.761 2.675h.656v-3.999h-.75v2.66h-.032l-1.752-2.66h-.662v4z" />
+                    d="M14 4.5V14a2 2 0 0 1-2 2v-1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5zm-3.76 8.132q.114.23.14.492h-.776a.8.8 0 0 0-.097-.249.7.7 0 0 0-.17-.19.7.7 0 0 0-.237-.126 1 1 0 0 0-.299-.044q-.427 0-.665.302-.234.301-.234.85v.498q0 .351.097.615a.9.9 0 0 0 .304.413.87.87 0 0 0 .519.146 1 1 0 0 0 .457-.096.67.67 0 0 0 .272-.264q.09-.164.091-.363v-.255H8.82v-.59h1.576v.798q0 .29-.097.55a1.3 1.3 0 0 1-.293.458 1.4 1.4 0 0 1-.495.313q-.296.111-.697.111a2 2 0 0 1-.753-.132 1.45 1.45 0 0 1-.533-.377 1.6 1.6 0 0 1-.32-.58 2.5 2.5 0 0 1-.105-.745v-.506q0-.543.2-.95.201-.406.582-.633.384-.228.926-.228.357 0 .636.1.281.1.48.275.2.176.314.407Z"/>
                 </svg>
                 <div class="file-spacer"></div>
                 <span class="file-name-photo file-name"><?php echo get_phrase('choose_a_file'); ?>...</span>
               </label>
-              <input id="student_image" type="file" class="inputfile" name="student_image" accept=".jpg, .jpeg, .png"
-                required>
+              <input id="student_image" type="file" class="inputfile" name="student_image" accept=".jpg, .jpeg, .png" required>
             </div>
-
           </div>
         </div>
+
         <!-- End Input -->
 
 
@@ -917,6 +914,19 @@
       }
     });
   }
+
+  document.getElementById('student_image').addEventListener('change', function(event) {
+  const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      const preview = document.getElementById('photo-preview');
+      preview.innerHTML = '<img src="' + e.target.result + '" alt="Photo preview" />';
+    };
+    reader.readAsDataURL(file);
+  }
+});
+
 </script>
 
 
