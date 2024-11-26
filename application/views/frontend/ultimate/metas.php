ba89698589b3bf36cf6bb19019043eb3c78569a3
allamdecloedt <abdelfattah.allam@decloedt.cloud>
2024-11-26 09:57:34 +0100

    SM-master  #comment  add icon navigateur

--- application/views/frontend/ultimate/metas.php
+++ application/views/frontend/ultimate/metas.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8" />
-    <title><?php echo get_phrase($page_title); ?></title>
+    <title><?php echo get_settings('system_title'); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Wayo Academy offers the best tools for learning, training, and skill development.">
     <meta name="keywords" content="Wayo Academy, online learning, training, coaching ,school">
@@ -8,4 +8,4 @@
     <meta name="google-site-verification" content="K7DuMo26Y6ocODe_Gv18MDsypa_06Zmr--CzQ_XVy-8" />
 
     <!-- App favicon -->
-<link rel="shortcut icon" href="<?php echo $this->settings_model->get_favicon(); ?>">
+<link rel="shortcut icon" href="<?php echo base_url('uploads/images/decloedt/logo/wayo_acadmy.png'); ?>">
