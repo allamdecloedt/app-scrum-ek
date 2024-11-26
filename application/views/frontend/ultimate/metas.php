fdfe6fa16b51c3da7368be2325511e910c14dec3
allamdecloedt <abdelfattah.allam@decloedt.cloud>
2024-11-26 10:01:02 +0100

    SM-master  #comment  add icon navigateur v1

--- application/views/frontend/ultimate/metas.php
+++ application/views/frontend/ultimate/metas.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8" />
-    <title><?php echo get_settings('system_title'); ?></title>
+    <title><?php echo get_settings('system_title') ." | ".  get_phrase($page_title); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Wayo Academy offers the best tools for learning, training, and skill development.">
     <meta name="keywords" content="Wayo Academy, online learning, training, coaching ,school">
