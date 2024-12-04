46d746c49f774dcd238fc65ac48bb38c682be767
fettah1949 <fattahallam1949@gmail.com>
2024-12-04 16:21:18 +0100

    SM-master  #comment  fix ifram v2

--- application/views/lessons/index.php
+++ application/views/lessons/index.php
@@ -1,3 +1,9 @@
+<?php
+// Désactivation du cache
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -7,6 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
     <meta content="Creativeitem" name="author" />
+	
 
     <!-- App favicon -->
 	<link rel="shortcut icon" href="<?php echo $this->settings_model->get_favicon(); ?>">
