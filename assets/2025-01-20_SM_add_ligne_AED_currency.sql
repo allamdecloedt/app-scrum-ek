0e1944893a67a310017e507fbdea93567886f652
fettah1949 <fattahallam1949@gmail.com>
2025-01-20 16:06:50 +0100

    SM-337  #comment add curiency AED

---
+++ assets/2025-01-20_SM_add_ligne_AED_currency.sql
@@ -0,0 +1,2 @@
+INSERT INTO `currencies` (`id`, `name`, `code`, `symbol`, `paypal_supported`, `stripe_supported`, `paystack_supported`, `payumoney_supported`) 
+VALUES (NULL, 'dirham émirati', 'AED', 'د.إ', '1', '1', '0', '1');
